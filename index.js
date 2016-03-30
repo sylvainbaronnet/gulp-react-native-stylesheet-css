a0625ef2b668817259ec7cec53298792861fd073
Sylvain Baronnet <sylvain@studio.gd>
2016-03-31 01:04:10 +0200

    .

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
 var through = require("through2"),
-	gutil = require("gulp-util"),
-	parseCss = require('./lib/parseCss');
+  gutil = require("gulp-util"),
+  path = require("path"),
+  parseCss = require('./lib/parseCss');
 
 var ext = gutil.replaceExtension;
 
 module.exports = function (options) {
-	"use strict";
-	options = options || {};
+  "use strict";
+  options = options || {};
 
-	function reactNativeCss(file, enc, callback) {
-		/*jshint validthis:true*/
+  function reactNativeCss(file, enc, callback) {
+    /*jshint validthis:true*/
 
-		// Do nothing if no contents
-		if (file.isNull()) {
-			this.push(file);
-			return callback();
-		}
+    // Do nothing if no contents
+    if (file.isNull()) {
+      this.push(file);
+      return callback();
+    }
 
-		if (file.isStream()) {
-			// accepting streams is optional
-			this.emit("error",
-				new gutil.PluginError("gulp-react-native-sass", "Stream content is not supported"));
-			return callback();
-		}
+    if (file.isStream()) {
+      // accepting streams is optional
+      this.emit("error",
+        new gutil.PluginError("gulp-react-native-sass", "Stream content is not supported"));
+      return callback();
+    }
 
-		// check if file.contents is a `Buffer`
-		if (file.isBuffer()) {
-			var source = file.contents.toString();
+    // check if file.contents is a `Buffer`
+    if (file.isBuffer()) {
+      var source = file.contents.toString();
 
-			var style = parseCss(source.replace(/\r?\n|\r/g, ""));
+      var style = parseCss(source.replace(/\r?\n|\r/g, ""));
 
-			var prefix = "", suffix = "";
-			if (options.outputPlainObject) {
-				prefix = "module.exports = ";
+      var prefix = "", suffix = "";
+      if (options.outputPlainObject) {
+        prefix = "module.exports = ";
       } else {
         var moduleName = "react-native"
         var objectName = "StyleSheet"
@@ -42,19 +43,27 @@ module.exports = function (options) {
           objectName = "EStyleSheet"
         }
 
-				prefix = "var " + objectName + " = require('" + moduleName + "')." + objectName + ";\nmodule.exports = " + objectName + ".create(";
-				suffix = ");";
+        var filename = path.basename(file.path)
+
+        prefix = `/**
+ * @providesModule ${filename}
+ */
+import ${objectName} from '${moduleName}'
+if(!EStyleSheet.builded){EStyleSheet.build({})}
+export default ${objectName}.create(`;
+
+        suffix = ")";
       }
 
-			file.contents = new Buffer(prefix + style + suffix);
+      file.contents = new Buffer(prefix + style + suffix);
 
-			file.path = ext(file.path, '.js');
+      file.path = ext(file.path, '.js');
 
-			this.push(file);
+      this.push(file);
 
-		}
-		return callback();
-	}
+    }
+    return callback();
+  }
 
-	return through.obj(reactNativeCss);
+  return through.obj(reactNativeCss);
 };
